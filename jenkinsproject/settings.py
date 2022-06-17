3a457ac7b17af74f0ed2621e2c8e34cff4122228
Rahul Jadhav <jrahul1112@gmail.com>
2022-06-17 19:20:04 +0530

    Second Commit

--- jenkinsproject/settings.py
+++ jenkinsproject/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'pingpong',
+    'django_jenkins',
 ]
 
 
@@ -86,3 +87,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_jslint',
+    'django_jenkins.tasks.run_csslint',
+    'django_jenkins.tasks.run_sloccount'
+)
