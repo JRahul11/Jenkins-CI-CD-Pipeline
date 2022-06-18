5cace4352625d704adfd5605c528f00a248711db
Rahul Jadhav <jrahul1112@gmail.com>
2022-06-18 19:46:12 +0530

    Added Welcome API

--- pingpong/urls.py
+++ pingpong/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('', Welcome.as_view()),
+    
     path('pingpong/', PingPong.as_view()),
 ]
--- pingpong/views.py
+++ pingpong/views.py
@@ -1,6 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+class Welcome(APIView):
+    
+    def get(self, request):
+        return Response("Welcome to Jenkins CI/CD Pipeline by Rahul J")
+
 class PingPong(APIView):
 
     def post(self, request):
